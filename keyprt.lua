1bb688cc38a8427dab0920509fc0a3d4000b77f6
newbie0z-lol <anhduyn937@gmail.com>
2025-09-14 17:00:11 +0700

    keyprt.lua

--- keyprt.lua
+++ keyprt.lua
@@ -1,12 +1,3 @@
-
-if game.PlaceId == 286090429 then
-    -- Arsenal
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/asenal/refs/heads/main/rezcdvn.lua"))()
-
-elseif game.PlaceId == 18192562963 then
-    -- Cộng Đồng Việt Nam
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/checkkeypremium/refs/heads/main/checkkeypre.lua"))()
-
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newbie0z-lol/preprt/refs/heads/main/preprt.lua"))()
 
 -- Ô lạc à chú em , đi về đi dumper hay spy cũng không có gì cho mày chơi đâu xéo dùm cái :]] yêu :33
